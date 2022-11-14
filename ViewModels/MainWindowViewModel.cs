0d941faba3034c47866e2973888b022dd78ba845
yuzhi535 <zhouyuxi.abc@gmail.com>
2022-11-14 11:07:12 +0800

    add new music store

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,26 @@
-﻿namespace avalonia_learning.ViewModels
+﻿using Avalonia;
+using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Windows.Input;
+
+
+namespace avalonia_learning.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+
+        public ICommand BuyMusicCommand { get; }
+
+
+        public MainWindowViewModel()
+        {
+            BuyMusicCommand = ReactiveCommand.Create(() => { });
+        }
     }
 }
